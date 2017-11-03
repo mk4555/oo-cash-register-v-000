cc8898d6135f63a6070a2324b8af1d6d26429430
Min Kim <mk4555@nyu.edu>
2017-11-03 16:32:34 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,9 +2,7 @@ class CashRegister
   attr_accessor :total, :employee_discount
 
   def initialize(discount)
-    cash_register = self.new
     @total = 0
     @discount = discount
-    cash_register
   end
 end
