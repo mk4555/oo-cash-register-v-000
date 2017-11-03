a6ac60b91627ddd8d8480453f05f8acbab73140c
Min Kim <mk4555@nyu.edu>
2017-11-03 16:58:24 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,6 +12,7 @@ class CashRegister
   def add_item(name, price, quantity = 1)
     @total += price * quantity
     @items.fill(name, @items.size, quantity)
+    @last = price * quantity
   end
 
   def apply_discount
