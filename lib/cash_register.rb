77cb97c614e106bd681df135a3a83277a66d61a7
Min Kim <mk4555@nyu.edu>
2017-11-03 16:44:42 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,4 @@
+require("pry")
 class CashRegister
   attr_accessor :total, :discount
 
@@ -14,6 +15,7 @@ class CashRegister
     discount = @discount / 100
     discount = @total * discount
     @total -= discount
+    
     "After the discount, the total comes to $#{@total}."
   end
 end
