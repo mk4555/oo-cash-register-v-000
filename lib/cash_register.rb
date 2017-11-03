19e4820cde3473e415df550f19e12167585a44a3
Min Kim <mk4555@nyu.edu>
2017-11-03 16:40:00 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
     @total += price * quantity
   end
 
-  def apply_discount()
+  def apply_discount(discount)
     @total -= @discount
     "After the discount, the total comes to $#{@total}."
   end
